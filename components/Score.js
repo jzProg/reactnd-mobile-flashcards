9426105c62c68f545536ed9de3e9493a155bac31
jzProg <john_z94@hotmail.com>
2020-12-06 22:42:19 +0200

    feat: score page update

--- components/Score.js
+++ components/Score.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 
-function Score({ route }) {
+function Score({ route, navigation }) {
+
+  function toHome() {
+    navigation.navigate('DeckListView');
+  }
+
+  function restart() {
+    navigation.navigate('Quiz');
+  }
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Score: {route.params.score}</Text>
+      <Button title="Restart Quiz" onPress={restart}/>
+      <Button title="to Home" onPress={toHome}/>
     </View>
     )
 }
