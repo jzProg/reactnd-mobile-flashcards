f24402a9b9c55218ab025796804b30b1a66c20ad
jzProg <john_z94@hotmail.com>
2020-12-13 22:30:03 +0200

    bug: animation issue fix

--- components/Deck.js
+++ components/Deck.js
@@ -5,7 +5,7 @@ import { styles } from '../utils/styles';
 function Deck({ title, cards, font, selected }) {
   return (
       <View style={styles.centered}>
-        <Animated.Text style={[styles.title, { fontSize: font && title === selected ? font : 20 }]}>
+        <Animated.Text style={[styles.title, { fontSize: (font && title === selected) ? font : 20 }]}>
           {title}
         </Animated.Text>
         <Text style={styles.italic}>
--- components/DeckListView.js
+++ components/DeckListView.js
@@ -21,21 +21,24 @@ class DeckListView extends Component {
    }
 
    fetchInitialData = () => {
-     getDecks().then((decks) => {
+     getDecks().then(decks => {
         this.setState({ decks, selected: '' });
      });
    }
 
    onDeckPress = (deck) => {
+     const { fontSize } = this.state;
      this.setState({ selected: deck.title });
-     Animated.spring(this.state.fontSize, { toValue: 40, speed: 5, useNativeDriver: false }).start();
-     this.props.navigation.navigate('Deck', { deckId: deck.title });
+     Animated.timing(fontSize, { toValue: 40, duration: 300, useNativeDriver: false }).start(() => {
+       this.props.navigation.navigate('Deck', { deckId: deck.title });
+       fontSize.setValue(20); // reset animation
+     });
    }
 
    render() {
      const { decks, fontSize, selected } = this.state;
      const deckArray = Object.values(decks);
-     
+
      return (
        !deckArray.length ? (
          <View style={styles.centered}>
@@ -43,7 +46,7 @@ class DeckListView extends Component {
          </View>
         ) : (
          <ScrollView>
-          { Object.values(this.state.decks).map(deck =>
+          { deckArray.map(deck =>
                <TouchableOpacity onPress={() => this.onDeckPress(deck)}
                                  key={deck.title}
                                  style={[{ height: 200 }, styles.card]}>
